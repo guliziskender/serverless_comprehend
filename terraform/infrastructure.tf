55b9a43b85f50b75ed263bf3ccf8ef4c0051be92
zeynep iskender <zeynep.iskender@mhp.com>
2024-03-22 11:35:50 +0100

    formatted terraform

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -1,11 +1,11 @@
 resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
   name        = "kinesis-firehose-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
   destination = "extended_s3"
-#  cloudwatch_logging_options {
-#    enabled = true
-#        log_group_name = data.aws_region.current.name/firehose/log-group
-#      }
-#  }
+  #  cloudwatch_logging_options {
+  #    enabled = true
+  #        log_group_name = data.aws_region.current.name/firehose/log-group
+  #      }
+  #  }
 
   extended_s3_configuration {
     role_arn   = aws_iam_role.firehose_role.arn
@@ -29,8 +29,6 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "kinesis-firehose-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
 }
-
-## TODO: Solve this issue
 
 resource "aws_s3_bucket_ownership_controls" "mybucket2-acl-ownership" {
   bucket = aws_s3_bucket.bucket.id
@@ -58,10 +56,10 @@ data "aws_iam_policy_document" "firehose_assume_role" {
 }
 
 resource "aws_iam_policy" "iam_policy_for_firehose" {
- name         = "aws_iam_policy_for_firehose"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_firehose"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -83,10 +81,10 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_firehose_log" {
- name         = "aws_iam_policy_for_firehose_logging"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_firehose_logging"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -105,10 +103,10 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_firehose_function" {
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -162,10 +160,10 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
 
- name         = "aws_iam_policy_for_lambda"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_lambda"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -185,10 +183,10 @@ EOF
 
 resource "aws_iam_policy" "iam_policy_for_lambda_service" {
 
- name         = "aws_iam_policy_for_lambda_service"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_lambda_service"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -210,10 +208,10 @@ EOF
 
 resource "aws_iam_policy" "iam_policy_for_lambda_twitter" {
 
- name         = "aws_iam_policy_for_lambda_service_twitter"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+  name        = "aws_iam_policy_for_lambda_service_twitter"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -283,7 +281,7 @@ resource "aws_lambda_function" "lambda_get_data" {
   layers        = ["arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p38-boto3:15", "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-tweepy:1"]
 
   vpc_config {
-  subnet_ids         = [module.vpc.public_subnets[0]]
-  security_group_ids = [aws_security_group.allow_https.id]
-}
+    subnet_ids         = [module.vpc.public_subnets[0]]
+    security_group_ids = [aws_security_group.allow_https.id]
+  }
 }
